991f38839d0f10cbf8b0d6826fb59ab0f1bdd0a5
amevigbe <yaoisidore.amevigbe.etu@univ-lille.fr>
2021-08-28 15:29:25 +0200

    mise à jour

---
+++ app.js
@@ -0,0 +1,153 @@
+const express = require('express');
+
+
+const app = express();
+const path = require('path');
+const http = require('http');
+
+
+const server = http.createServer(app);
+
+
+let rooms = [];
+const {Server} = require('socket.io');
+
+const io = new Server(server);
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+
+app.use('/bootstrap/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/bootstrap/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/jquery', express.static(path.join(__dirname, 'node_modules/jquery/dist')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+ 
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + "/templates/" + "index.html");
+  });
+
+
+  app.get('/games/morpion', function(req, res) {
+    res.sendFile(__dirname + "/templates/games/" + "morpion.html");
+    });
+
+
+    app.post('/games/morpion', function(req, res) {
+        var user_name = req.body.username;
+        //console.log(user_name);
+        res.end("yes");
+        });
+
+
+    const roomId = ()=>{
+        return Math.random().toString(36).substring(2,9);
+    }
+    const createRoom = player =>{
+        let room = {roomId : roomId(), players : []};
+        player.roomId = room.roomId;
+        room.players.push(player);
+        rooms.push(room);
+        return room;
+    };
+
+    
+    io.on('connection' , (socket)=>{
+        console.log(`${socket.id} est connecté`);
+        //console.log(roomId());
+        socket.on('playerData' , (player)=>{
+            console.log(`${player.username} vient d'être en ligne`);
+            let room = null;
+
+            if(player.roomId === null){
+                room = createRoom(player);
+                console.log(`le joueur ${player.username} est dans le salon ${player.roomId}`)
+                //console.log(player);
+                //socket.emit("join", player);
+            }
+            else{
+                room = rooms.find(element => element.roomId === player.roomId);
+                //console.log(room);
+                if(room === null){
+                    console.log('Pas de salon trouvé');
+                    return false;
+                }
+                
+                room.players.push(player);
+                
+            }
+            
+            socket.join(player.roomId);
+            io.sockets.to(socket.id).emit('join room', player.roomId);
+           // console.log(room);
+            if(room.players.length === 2){
+                
+                io.sockets.to(room.roomId).emit('start game', room.players);
+                
+
+            }
+            socket.on('play', (player)=>{
+                /**
+                 * 
+                 * socket.on("join", (joueur) =>{
+                    player.roomId = joueur.roomId});
+                **/
+                //player.roomId = room.roomId;
+                io.sockets.to(room.roomId).emit('play', player);
+                //console.log(player);
+            });
+
+            socket.on('play again', (roomId)=>{
+                room = rooms.find(r => r.roomId === roomId);
+                if (room && room.players.length === 2){
+                    io.sockets.to(room.roomId).emit('playAgain', room.players);
+                }
+            })
+            
+        }
+
+
+        
+        
+        
+        )
+        
+        socket.on('get rooms', ()=>{
+            //console.log('jeeeeeeeeeeeeeeeeee');
+            socket.emit('list room', rooms);
+        });
+        
+   socket.on('disconnect', ()=>{
+            console.log(`l'utilisateur ${socket.id} déconnecté`);
+            let room = null;
+
+            rooms.forEach( element => {
+                element.players.forEach(
+                    r => {
+                        if (r.socketId === socket.id && r.host){
+                            room = element;
+                            let t =[];
+                           rooms.forEach(
+                               elt=>{
+                                   if (elt !== room){
+                                       t.push(elt);
+                                   }
+                               }
+                           );
+                           rooms = t;
+                        }
+                    }
+                )
+                
+            });
+        });
+
+        console.log(rooms);
+    });
+
+    
+
+
+ server.listen((3000),()=>{
+     console.log('Listen on https:\\localhost:3000/')
+ });   
---
+++ public/js/index.js
@@ -0,0 +1,375 @@
+window.addEventListener('load',initForm);
+var socket = io();
+
+
+
+const player={
+    host : '',
+    playerCell : '',
+    roomId : null,
+    username : '',
+    socketId : '',
+    symbol : "X",
+    turn : false,
+    win : false
+}
+
+
+
+
+function initForm(){
+  
+  document.forms.form.addEventListener("submit", sendForm1);
+  //.forms.form.addEventListener("submit", sendForm2);
+  
+}
+
+
+
+let roomsCard = document.getElementById("rooms-card");
+let roomsList = document.getElementById("rooms-list");
+let restartarea = document.getElementById("restart-area");
+let message = document.getElementById("turn-message");
+let game = document.getElementById("game-card");
+
+let link = document.getElementById("link");
+let linkId = document.getElementById("link-to-share");
+    let usercard = document.getElementById("user-card");
+    const usernameJ = document.getElementById("username");
+//var socket = io();
+let wattingarea = document.getElementById("waiting-area");
+
+
+restartarea.addEventListener('click', ()=>restartGame());
+function sendForm1(ev){
+
+    ev.preventDefault();
+    let username = document.getElementById("username");
+    let usercard = document.getElementById("user-card");
+    let watting = document.getElementById("rooms-card");
+    
+    
+    player.username = username.value;
+    
+    player.turn =true;
+    player.host = true;
+    player.socketId = socket.id;
+    usercard.hidden = true;
+    watting.classList.remove("d-none");
+    wattingarea.classList.remove("d-none");
+
+    
+
+
+
+socket.emit(
+    'playerData', 
+        player,
+  )}
+  
+  ;
+
+  socket.on('join room', (data)=>{
+    linkId.innerHTML += `<a href="${window.location.href}?room=${data}" target="_blank">${window.location.href}?room=${data}</a>`;
+    player.roomId = data;
+  })
+
+
+
+socket.emit('get rooms');
+
+
+let cells = document.getElementsByClassName("cell");
+
+
+function play(ev){
+   //console.log(player);
+    //console.log(this.innerText === "" && player.turn);
+    let id=  this.getAttribute("id");
+    
+    if (this.innerText === "" && player.turn){
+     //player.win = calculateWin(id);
+     player.playerCell = id;
+     player.turn = false;
+     this.innerHTML = player.symbol;
+     
+     socket.emit("play", player);
+    }
+   // console.log(player)
+    
+ };
+
+for (let cell of cells){
+cell.addEventListener("click", play)};
+
+
+
+socket.on('play', (ennemyPlayer) => {
+    console.log(player);
+    if (ennemyPlayer.socketId !== player.socketId){
+       // console.log("jeeeeeeeeeeeeeee1");
+        const playerCell = document.getElementById(`${ennemyPlayer.playerCell}`);
+        playerCell.classList.add("text-danger");
+        playerCell.innerHTML+="O";
+        let win = calculateWin(playerCell.getAttribute("id"), "O");
+        if (win){
+            ennemyPlayer.win = true;
+        }
+        if (ennemyPlayer.win){
+            setMessage("alert-info", "alert-danger", `C'est ${ennemyPlayer.username} qui vient de gagner`);
+            
+        }
+
+        else if (calculateEquality()){
+            setMessage("alert-info", "alert-warning", `C'est l'égalité`);
+        }
+
+        //console.log(playerCell.getAttribute("id"));
+
+
+        
+
+        else{
+
+    setMessage('alert-info', 'alert-success', "c'est ton tour de jouer");
+    player.turn = true;}
+
+    }
+
+    else{
+        //console.log("jeeeeeeeeeeeeeeeeeeeee2");
+        const playerCell = document.getElementById(`${player.playerCell}`);
+        let win = calculateWin(playerCell.getAttribute("id"), "X");
+        if (win){
+            player.win = true;
+        }
+        if (player.win){
+            setMessage("alert-info", "alert-success", `Félicitations vous avez gagné`);
+            //calculateWin(playerCell.playerCell, "");
+        }
+
+        else if (calculateEquality()){
+            setMessage("alert-info", "alert-warning", `C'est l'égalité`);
+        }
+
+       else{ setMessage('alert-success', 'alert-info', `c'est le tour de ${ennemyPlayer.username} tour de jouer`);
+        player.turn = false;
+    }
+
+    }
+
+   
+});
+
+
+socket.on('list room', rooms=>{
+    //console.log(rooms);
+
+    let html = "";
+
+
+    if (rooms.length > 0){
+        //console.log("jeeeeeeeeeeeeeeeeee");
+        rooms.forEach(element => {
+            if (element.players.length !== 2){
+                html += `<li class="list-group-item d-flex justify-content-between">
+                            <p class="p-0 m-0 flex-grow-1 fw-bold">Salon de ${element.players[0].username} - ${element.roomId}</p>
+                            <button class="btn btn-sm btn-success join-room" data-room="${element.roomId}">Rejoindre</button>
+                        </li>`
+            }
+            
+        });
+    }
+
+    if (html !== ""){
+        roomsCard.classList.remove('d-none');
+        roomsList.innerHTML = html;
+    }
+
+    for (let element of document.getElementsByClassName("join-room")){
+        element.addEventListener("click", joinRoom);
+ 
+    }
+});
+
+function startgame(players){
+    //console.log("jeeeeeeeeeeeeeeeeee");
+    
+    message.classList.remove("d-none");
+    game.classList.remove("d-none");
+    wattingarea.classList.add("d-none");
+    restartarea.classList.remove("d-none");
+    link.hidden= true;
+    roomsCard.classList.add("d-none");
+    let ennemie = players.find(r => r.socketId !== player.socketId);
+    let ennemieusername = ennemie.username;
+    if (player.host && player.turn){
+        setMessage('alert-info', 'alert-success', "C'est ton tour de jouer");
+    }
+    else{
+        setMessage( 'alert-success','alert-info', `C'est le tour de ${ennemieusername} tour de jouer`);
+    }
+}
+
+socket.on("start game", (players)=>{
+    startgame(players);
+});
+
+
+function setMessage(classa, classb, messageT){
+    message.classList.remove(classa);
+    message.classList.add(classb);
+    
+    message.innerHTML = messageT;
+};
+
+
+function joinRoom(ev){
+
+    
+    if (usernameJ.value !== ""){
+        player.username = usernameJ.value;
+        player.roomId = this.dataset.room;
+        
+        player.socketId = socket.id;
+
+
+        socket.emit('playerData', player);
+        usercard.hidden = true;
+        
+        roomsCard.classList.add("d-none");
+        wattingarea.classList.remove("d-none");
+        
+
+    }
+}
+
+function calculateEquality(){
+    res = true;
+    for (let cell of document.getElementsByClassName("cell")){
+        console.log(cell.innerHTML);
+        if (cell.innerText === ""){
+            
+            res = false;
+        }
+    }
+    return res;
+}
+
+function restartGame(players = null){
+    if (player.host && !players){
+        console.log(player.roomId);
+        player.turn  = true;
+        socket.emit('play again', player.roomId);
+    }
+
+    const cell = document.getElementsByClassName("cell");
+
+    for (let c of cell){
+        c.innerHTML = "";
+        c.classList.remove("win-cell");
+
+    }
+    message.classList.remove('alert-danger');
+    if (!player.host){
+        player.turn = false;
+    }
+
+    player.win = false;
+    if (players){
+        console.log(players);
+        startgame(players);
+    }
+}
+
+socket.on('playAgain', (data)=>{
+    console.log("play Again");
+    restartGame(data);
+})
+
+
+function calculateWin(playedCell, symbol = player.symbol) {
+    
+
+    colonne = playedCell[playedCell.length-1];
+    ligne = playedCell[playedCell.length-3];
+    // 1) VERTICAL (check if all the symbols in clicked cell's column are the same)
+    let win = true;
+
+    for (let i = 1; i < 4; i++) {
+        
+        let cell = document.getElementById(`cell-${i}-${parseInt(colonne)}`);
+        console.log(cell.value);
+        if (cell.textContent !== symbol) {
+            win = false;
+        }
+    }
+
+    if (win) {
+        //column = 1;
+        
+        for (let i = 1; i < 4; i++) {
+            let cell = document.getElementById(`cell-${i}-${parseInt(colonne)}`);
+            cell.classList.add("win-cell");
+        }
+
+        return win;
+    }
+
+    // 2) HORIZONTAL (check the clicked cell's row)
+    
+    win = true;
+    for (let i = 1; i < 4; i++) {
+        let cell = document.getElementById(`cell-${parseInt(ligne)}-${i}`);
+        if (cell.textContent !== symbol) {
+            win = false;
+        }
+    }
+
+    if (win) {
+        for (let i = 1; i < 4; i++) {
+            let cell = document.getElementById(`cell-${parseInt(ligne)}-${i}`);
+            cell.classList.add("win-cell");
+        }
+
+        return win;
+    }
+
+    // 3) MAIN DIAGONAL (for the sake of simplicity it checks even if the clicked cell is not in the main diagonal)
+
+    win = true;
+
+    for (let i = 1; i < 4; i++) {
+        let cell = document.getElementById(`cell-${parseInt(i)}-${i}`);
+        if (cell.textContent !== symbol) {
+            win = false;
+        }
+    }
+
+    if (win) {
+        for (let i = 1; i < 4; i++) {
+            let cell = document.getElementById(`cell-${parseInt(i)}-${i}`);
+            cell.classList.add("win-cell");
+        }
+
+        return win;
+    }
+
+    // 3) SECONDARY DIAGONAL
+
+    win = false;
+    
+    if (document.getElementById("cell-1-3").textContent === symbol) {
+        if (document.getElementById("cell-2-2").textContent === symbol) {
+            if (document.getElementById("cell-3-1").textContent === symbol) {
+                win = true;
+
+                document.getElementById("cell-1-3").classList.add("win-cell");
+                document.getElementById("cell-2-2").classList.add("win-cell");
+               document.getElementById("cell-3-1").classList.add("win-cell");
+
+                return win;
+            }
+        }
+    }
+}
